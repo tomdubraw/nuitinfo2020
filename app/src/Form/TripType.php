8769b519b7623896456b007844d16a01cd5c7df5
JeremTech <jeremtech@gmx.fr>
2020-12-04 03:31:11 +0100

    Form style trip (#14)
    * Patch de la navbar qui cache le haut du contenu
    
    * Formulaire + fixe navbar
    
    * Fix footer + amélioration formulaire
    
    Co-authored-by: Thomas <thomas@thomasdubreuil.com>

--- app/src/Form/TripType.php
+++ app/src/Form/TripType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Trip;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +16,40 @@ class TripType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('city')
-            ->add('starttime')
-            ->add('endtime')
-            ->add('batherNumber')
-            ->add('boatNumber')
-            ->add('fishingBoat')
-            ->add('pleasureBoat')
-            ->add('sailboat')
-            ->add('surferNumber')
-            ->add('pollutionCommentary')
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+            ])
+            ->add('starttime', DateTimeType::class, [
+                'label' => 'Début de séance',
+                'data' => new \DateTime(),
+            ])
+            ->add('endtime', DateTimeType::class, [
+                'label' => 'Fin de séance',
+                'data' => new \DateTime(),
+            ])
+            ->add('batherNumber', IntegerType::class, [
+                'label' => 'Nombre de baigneurs',
+                'data' => 0,
+            ])
+            ->add('fishingBoat', IntegerType::class, [
+                'label' => 'Nombre de bateaux de pêche',
+                'data' => 0,
+            ])
+            ->add('pleasureBoat', IntegerType::class, [
+                'label' => 'Nombre de bateaux de plaisance',
+                'data' => 0,
+            ])
+            ->add('sailboat', IntegerType::class, [
+                'label' => 'Nombre de bateaux à voile',
+                'data' => 0,
+            ])
+            ->add('surferNumber', IntegerType::class, [
+                'label' => 'Nombre de surfeurs',
+                'data' => 0,
+            ])
+            ->add('pollutionCommentary', TextareaType::class, [
+                'label' => 'Observations',
+            ])
         ;
     }
 
