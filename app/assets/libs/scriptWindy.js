6b07f5317875feed612b834e2dce7b4ef649623e
Clément Dubreuil <hello@clementd.fr>
2020-12-04 07:40:25 +0100

    Script windy v2 (#26)
    * Update master (readme) (#3)
    
    * fix log nginx
    
    * Readme Update (#2)
    
    * Update
    
    * Command
    
    * Typo
    
    * Add info
    
    Co-authored-by: Clément Dubreuil <hello@clementd.fr>
    
    * init windyCarte
    
    * Carte en grand avec message
    
    * C'est compliqué le refactor
    
    * up le titre
    
    * route : /windy
    
    * nom de la fonction
    
    * Update scriptWindy.js
    
    Modification commentaires
    
    * Correction Controller
    Ajout WindyController.php
    
    * Commentaires imports
    
    * Commentaires imports
    
    * Update SecurityController.php
    
    J'enlève de l'espace
    
    * J'ai remis le stylesheet
    
    * Prod de 4h (#17)
    
    * first entities (#8)
    
    * first entities
    
    * Link between user and Waterman/organization
    
    * Fix user fixture
    
    * Trip form base (#9)
    
    * first entities
    
    * Form type
    
    * Link between user and Waterman/organization
    
    * Fix user fixture
    
    * Form
    
    * Working form
    
    * Merge header/footer design (#11)
    
    * Navbar et footer de base
    
    * Finitions sur la navbar et les couleurs
    
    Co-authored-by: JeremTech <jeremtech@gmx.fr>
    
    * Windy carte (#10)
    
    * Update master (readme) (#3)
    
    * fix log nginx
    
    * Readme Update (#2)
    
    * Update
    
    * Command
    
    * Typo
    
    * Add info
    
    Co-authored-by: Clément Dubreuil <hello@clementd.fr>
    
    * init windyCarte
    
    * Carte en grand avec message
    
    * C'est compliqué le refactor
    
    * up le titre
    
    * route : /windy
    
    * nom de la fonction
    
    * Update scriptWindy.js
    
    Modification commentaires
    
    * Correction Controller
    Ajout WindyController.php
    
    * Commentaires imports
    
    * Commentaires imports
    
    * Update SecurityController.php
    
    J'enlève de l'espace
    
    * J'ai remis le stylesheet
    
    Co-authored-by: Thomas Dubreuil <57966843+tomdubraw@users.noreply.github.com>
    Co-authored-by: Clément Dubreuil <hello@clementd.fr>
    Co-authored-by: Thomas <thomas@thomasdubreuil.com>
    
    * Add map open data (#12)
    
    * Add map open data
    
    * Update OpenDataController.php
    
    * add leet speak (#15)
    
    * Form style trip (#14)
    
    * Patch de la navbar qui cache le haut du contenu
    
    * Formulaire + fixe navbar
    
    * Fix footer + amélioration formulaire
    
    Co-authored-by: Thomas <thomas@thomasdubreuil.com>
    
    * Index body (#16)
    
    * first entities
    
    * Form type
    
    * Page home
    
    * Création d'un user
    
    * Add password hashing
    
    * Formulaire de connexion ok
    
    Co-authored-by: Thomas <thomas@thomasdubreuil.com>
    Co-authored-by: sol0007 <guillaume.sol@etudiant.univ-reims.fr>
    
    Co-authored-by: JeremTech <jeremtech@gmx.fr>
    Co-authored-by: Adrades <32618947+Adrades@users.noreply.github.com>
    Co-authored-by: Clément Dubreuil <hello@clementd.fr>
    Co-authored-by: Guillaume <49609994+guiwil17@users.noreply.github.com>
    Co-authored-by: sol0007 <guillaume.sol@etudiant.univ-reims.fr>
    
    * update scriptWindy.js
    
    * remove commentaires scriptWindy.js
    
    * lol dans  scriptWindy.js
    
    * J'ai fais le flavicon
    
    * J'ai fais "découvert" une fonction distance
    
    * Les markers font une condition avec l'empreinte carborne!
    
    * Erreur 500 lors du parse
    
    * Pour clément
    
    * Pour clément
    
    * end windy map
    
    Co-authored-by: Thomas Dubreuil <57966843+tomdubraw@users.noreply.github.com>
    Co-authored-by: Thomas <thomas@thomasdubreuil.com>
    Co-authored-by: Adrades <genadrades@gmail.com>
    Co-authored-by: Adrades <32618947+Adrades@users.noreply.github.com>
    Co-authored-by: JeremTech <jeremtech@gmx.fr>
    Co-authored-by: Guillaume <49609994+guiwil17@users.noreply.github.com>
    Co-authored-by: sol0007 <guillaume.sol@etudiant.univ-reims.fr>

--- app/assets/libs/scriptWindy.js
+++ app/assets/libs/scriptWindy.js
@@ -19,34 +19,114 @@ const options = {
     graticule: false,
 };
 
+function distance(position1, position2) {
+    var lat1 = position1[0], lon1 = position1[1], lat2 = position2[0], lon2 = position2[1];
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1/180;
+        var radlat2 = Math.PI * lat2/180;
+        var theta = lon1-lon2;
+        var radtheta = Math.PI * theta/180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344
+        return dist;
+    }
+}
+
+function geoLocBase() {
+    var ObjPosition = new ol.Feature();
+    var geolocation = new ol.Geolocation({
+        tracking: true,
+    });
+
+    var getPosition = [49.24046277613983, 4.061726162584737, "Ancien local de l'équipe StackOverflow Driven Development..."];
+
+    try {
+        var gl = geolocation.getPosition();
+        getPosition[0] = gl[0];
+        getPosition[1] = gl[1];
+        getPosition[2] = "Votre Position";
+    } catch (error) {
+        console.log("Erreur de localisation!! ");
+    }
+
+    return getPosition;
+}
+
 /**
  *
  * @param listeDePoints Array
- * @param LL
- * @param M
  */
 function ajouterPoints(listeDePoints) {
     var markers = new L.layerGroup(listeDePoints.map(element => ajouterPoint(element, L)));
     return markers;
 }
 
-function ajouterPoint(dicoPoint){
+function ajouterPoint(dicoPoint) {
     return new L.marker([dicoPoint['lat'], dicoPoint['lon']]).bindPopup(dicoPoint['content'])
 }
 
+// var listeTestPoint = [{'lat': 48.76013, 'lon': 2.38690, 'content': "ICI c'est Thiais!"},
+//     {'lat': 14.00336, 'lon': 120.59463, 'content': "L'île dans une île  dans une île!"},
+//     {'lat': 1.00336, 'lon': 10.59463, 'content': "Lol"}]
 
-var listeTestPoint = [{'lat': 48.76013, 'lon': 2.38690, 'content': "ICI c'est Thiais"},
-    {'lat': 14.00336, 'lon': 120.59463, 'content': "L'île dans une île  dans une île"},
-    {'lat': 1.00336, 'lon': 10.59463, 'content': "Lol"}]
+var listeTestPoint = [];
 
 // Initialize Windy API
-windyInit(options, windyAPI => {
+windyInit(options, async windyAPI => {
     // windyAPI is ready, and contain 'map', 'store',
 
     const {store} = windyAPI;
 
     const {map} = windyAPI;
     // .map is instance of Leaflet map
+    var gl = geoLocBase(),str = "", dp = 0;
+
+    function getCoordFromAddress(address) {
+        const url = new URL("https://nominatim.openstreetmap.org/search");
+        url.searchParams.append("q", address);
+        url.searchParams.append("format", "json");
+        url.searchParams.append("limit", "1");
+
+        return fetch(url)
+            .then(r => r.json())
+            .then(e => L.latLng(Number(e[0].lat), Number(e[0].lon)));
+    }
+
+    function loadMarker() {
+        return fetch("/favcities")
+            .then(r => r.json())
+            .then(data => {
+                return Promise.all(data.map(async entry => {
+                    const c = await getCoordFromAddress(entry.city);
+                    return {'lat': c.lat, 'lon': c.lon, 'content': entry.city};
+                }));
+            });
+    }
+
+    for (point of await loadMarker()) {
+        dp = distance([gl[0], gl[1]], [point['lat'], point['lon']]).toFixed(2);
+        if(dp > 1000){
+            str = "Votre empreinte Carbone est très mauvaise!"
+        }else if (dp > 500){
+            str = "Votre empreinte Carbone est mauvaise."
+        }else if (dp > 100){
+            str = "Votre empreinte Carbone est moyenne."
+        }else{
+            str = "Votre empreinte Carbone est bonne!"
+        }
+        point['content'] += " Distance de la position: " +dp + "Km.  " + str;
+    }
+
+    listeTestPoint.push({'lat': gl[0], 'lon': gl[1], 'content': gl[2]});
 
     ajouterPoints(listeTestPoint).addTo(map);
 });
