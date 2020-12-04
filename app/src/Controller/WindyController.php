dc23a72e411a2636b134358a6206b3dee144980c
Thomas Dubreuil <57966843+tomdubraw@users.noreply.github.com>
2020-12-04 06:17:16 +0100

    Fav cities cal api (#21)

--- app/src/Controller/WindyController.php
+++ app/src/Controller/WindyController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\FavCity;
+use App\Repository\FavCityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WindyController extends AbstractController
@@ -13,5 +16,16 @@ class WindyController extends AbstractController
     public function windyRoute()
     {
         return $this->render('windyCarte/windyCarteFull.html.twig');
+    }
+
+    /**
+     * @Route("/favcities", name="favcities")
+     */
+    public function getFavCities(FavCityRepository $favCityRepository)
+    {
+        $response = new Response();
+        $response->setContent(json_encode($favCityRepository->getCities($this->getUser())));
+
+        return $response;
     }
 }
